3b6baa307f0112aeaa5023394d1893b36cd8aed5
hassan2206f <hassan2206f@aptechgdn.com>
2023-01-11 17:02:07 +0500

    javascript

---
+++ json.js
@@ -0,0 +1,91 @@
+var products = "[{\"title\":\"Browneggs\",\"type\":\"dairy\",\"description\":\"Raworganicbrowneggsinabasket\",\"filename\":\"0.jpg\",\"height\":600,\"width\":400,\"price\":28.1,\"rating\":4},{\"title\":\"Sweetfreshstawberry\",\"type\":\"fruit\",\"description\":\"Sweetfreshstawberryonthewoodentable\",\"filename\":\"1.jpg\",\"height\":450,\"width\":299,\"price\":29.45,\"rating\":4},{\"title\":\"Asparagus\",\"type\":\"vegetable\",\"description\":\"Asparaguswithhamonthewoodentable\",\"filename\":\"2.jpg\",\"height\":450,\"width\":299,\"price\":18.95,\"rating\":3},{\"title\":\"Greensmoothie\",\"type\":\"dairy\",\"description\":\"Glassofgreensmoothiewithquailegg'syolk,servedwithcocktailtube,greenappleandbabyspinachleavesovertinsurface.\",\"filename\":\"3.jpg\",\"height\":600,\"width\":399,\"price\":17.68,\"rating\":4},{\"title\":\"Rawlegums\",\"type\":\"vegetable\",\"description\":\"Rawlegumsonthewoodentable\",\"filename\":\"4.jpg\",\"height\":450,\"width\":299,\"price\":17.11,\"rating\":2},{\"title\":\"Bakingcake\",\"type\":\"dairy\",\"description\":\"Bakingcakeinruralkitchen-doughrecipeingredients(eggs,flour,sugar)onvintagewoodentablefromabove.\",\"filename\":\"5.jpg\",\"height\":450,\"width\":675,\"price\":11.14,\"rating\":4},{\"title\":\"Pestowithbasil\",\"type\":\"vegetable\",\"description\":\"Italiantraditionalpestowithbasil,chesseandoil\",\"filename\":\"6.jpg\",\"height\":450,\"width\":299,\"price\":18.19,\"rating\":2},{\"title\":\"Hazelnutinblackceramicbowl\",\"type\":\"vegetable\",\"description\":\"Hazelnutinblackceramicbowlonoldwoodenbackground.forestwealth.rusticstyle.selectivefocus\",\"filename\":\"7.jpg\",\"height\":450,\"width\":301,\"price\":27.35,\"rating\":0},{\"title\":\"Freshstawberry\",\"type\":\"fruit\",\"description\":\"Sweetfreshstawberryonthewoodentable\",\"filename\":\"8.jpg\",\"height\":600,\"width\":399,\"price\":28.59,\"rating\":4},{\"title\":\"Lemonandsalt\",\"type\":\"fruit\",\"description\":\"Rosemary,lemonandsaltonthetable\",\"filename\":\"9.jpg\",\"height\":450,\"width\":299,\"price\":15.79,\"rating\":5},{\"title\":\"Homemadebread\",\"type\":\"bakery\",\"description\":\"Homemadebread\",\"filename\":\"10.jpg\",\"height\":450,\"width\":301,\"price\":17.48,\"rating\":3},{\"title\":\"Legums\",\"type\":\"vegetable\",\"description\":\"Cookedlegumsonthewoodentable\",\"filename\":\"11.jpg\",\"height\":600,\"width\":399,\"price\":14.77,\"rating\":0},{\"title\":\"Freshtomato\",\"type\":\"vegetable\",\"description\":\"Freshtomatojuicewithbasil\",\"filename\":\"12.jpg\",\"height\":600,\"width\":903,\"price\":16.3,\"rating\":2},{\"title\":\"Healthybreakfast\",\"type\":\"fruit\",\"description\":\"Healthybreakfastset.ricecerealorporridgewithberriesandhoneyoverrusticwoodbackground\",\"filename\":\"13.jpg\",\"height\":450,\"width\":350,\"price\":13.02,\"rating\":2},{\"title\":\"Greenbeans\",\"type\":\"vegetable\",\"description\":\"Raworganicgreenbeansreadytoeat\",\"filename\":\"14.jpg\",\"height\":450,\"width\":300,\"price\":28.79,\"rating\":1},{\"title\":\"Bakedstuffedportabellomushrooms\",\"type\":\"bakery\",\"description\":\"Homemadebakedstuffedportabellomushroomswithspinachandcheese\",\"filename\":\"15.jpg\",\"height\":600,\"width\":400,\"price\":20.31,\"rating\":1},{\"title\":\"Strawberryjelly\",\"type\":\"fruit\",\"description\":\"Homemadeorganicstrawberryjellyinajar\",\"filename\":\"16.jpg\",\"height\":400,\"width\":600,\"price\":14.18,\"rating\":1},{\"title\":\"Pearsjuice\",\"type\":\"fruit\",\"description\":\"Freshpearsjuiceonthewoodentable\",\"filename\":\"17.jpg\",\"height\":600,\"width\":398,\"price\":19.49,\"rating\":4},{\"title\":\"Freshpears\",\"type\":\"fruit\",\"description\":\"Sweetfreshpearsonthewoodentable\",\"filename\":\"18.jpg\",\"height\":600,\"width\":398,\"price\":15.12,\"rating\":5},{\"title\":\"Capresesalad\",\"type\":\"vegetable\",\"description\":\"Homemadehealthycapresesaladwithtomatomozzarellaandbasil\",\"filename\":\"19.jpg\",\"height\":400,\"width\":600,\"price\":16.76,\"rating\":5},{\"title\":\"Oranges\",\"type\":\"fruit\",\"description\":\"Orangepopsicleicecreambarsmadefromfreshoranges.arefreshingsummertreat.\",\"filename\":\"20.jpg\",\"height\":450,\"width\":274,\"price\":21.48,\"rating\":4},{\"title\":\"Veganfood\",\"type\":\"vegetable\",\"description\":\"Conceptofveganfood\",\"filename\":\"21.jpg\",\"height\":450,\"width\":299,\"price\":29.66,\"rating\":4},{\"title\":\"Breakfastwithmuesli\",\"type\":\"dairy\",\"description\":\"Conceptofhealthybreakfastwithmuesli\",\"filename\":\"22.jpg\",\"height\":450,\"width\":299,\"price\":22.7,\"rating\":2},{\"title\":\"Honey\",\"type\":\"bakery\",\"description\":\"Honeyandhoneycellonthetable\",\"filename\":\"23.jpg\",\"height\":450,\"width\":299,\"price\":17.01,\"rating\":2},{\"title\":\"Breakfastwithcottage\",\"type\":\"fruit\",\"description\":\"Healthybreakfastwithcottagecheeseandstrawberry\",\"filename\":\"24.jpg\",\"height\":600,\"width\":398,\"price\":14.05,\"rating\":1},{\"title\":\"Strawberrysmoothie\",\"type\":\"fruit\",\"description\":\"Glassofredstrawberrysmoothiewithchiaseeds,servedwithretrococktailtube,freshmintandstrawberriesoverdarkbackground\",\"filename\":\"25.jpg\",\"height\":600,\"width\":400,\"price\":28.86,\"rating\":2},{\"title\":\"Strawberryandmint\",\"type\":\"fruit\",\"description\":\"Homemademuesliwithstrawberryandmint\",\"filename\":\"26.jpg\",\"height\":450,\"width\":299,\"price\":26.21,\"rating\":4},{\"title\":\"Ricotta\",\"type\":\"dairy\",\"description\":\"Ricottawithberryandmint\",\"filename\":\"27.jpg\",\"height\":600,\"width\":398,\"price\":27.81,\"rating\":5},{\"title\":\"Cubansandwiche\",\"type\":\"bakery\",\"description\":\"Homemadetraditionalcubansandwicheswithhamporkandcheese\",\"filename\":\"28.jpg\",\"height\":450,\"width\":300,\"price\":18.5,\"rating\":4},{\"title\":\"Granola\",\"type\":\"dairy\",\"description\":\"Glassjarwithhomemadegranolaandyogurtwithnuts,raspberriesandblackberriesonwoodencuttingboardoverwhitetextileindaylight\",\"filename\":\"29.jpg\",\"height\":450,\"width\":300,\"price\":29.97,\"rating\":3},{\"title\":\"Smoothiewithchiaseeds\",\"type\":\"fruit\",\"description\":\"Glassofredstrawberrysmoothiewithchiaseeds,servedwithretrococktailtube,freshmintandstrawberriesoverwoodentable\",\"filename\":\"30.jpg\",\"height\":600,\"width\":900,\"price\":25.26,\"rating\":5},{\"title\":\"Yogurt\",\"type\":\"dairy\",\"description\":\"Homemadeyogurtwithraspberryandmint\",\"filename\":\"31.jpg\",\"height\":450,\"width\":299,\"price\":27.61,\"rating\":4},{\"title\":\"Sandwichwithsalad\",\"type\":\"vegetable\",\"description\":\"Vegansandwichwithsalad,tomatoandradish\",\"filename\":\"32.jpg\",\"height\":600,\"width\":398,\"price\":22.48,\"rating\":5},{\"title\":\"Cherry\",\"type\":\"fruit\",\"description\":\"Cherrywithsugaronoldtable\",\"filename\":\"33.jpg\",\"height\":600,\"width\":400,\"price\":14.35,\"rating\":5},{\"title\":\"Rawasparagus\",\"type\":\"vegetable\",\"description\":\"Rawfreshasparagussaladwithcheeseanddressing\",\"filename\":\"34.jpg\",\"height\":600,\"width\":400,\"price\":22.97,\"rating\":4},{\"title\":\"Corn\",\"type\":\"vegetable\",\"description\":\"Grilledcornonthecobwithsaltandbutter\",\"filename\":\"35.jpg\",\"height\":450,\"width\":300,\"price\":13.55,\"rating\":2},{\"title\":\"Vegan\",\"type\":\"vegan\",\"description\":\"Conceptofhealthyveganeating\",\"filename\":\"36.jpg\",\"height\":600,\"width\":398,\"price\":28.96,\"rating\":5},{\"title\":\"Freshblueberries\",\"type\":\"fruit\",\"description\":\"Healthybreakfast.berrycrumblewithfreshblueberries,raspberries,strawberries,almond,walnuts,pecans,yogurt,andmintinceramicplatesoverwhitewoodensurface,topview\",\"filename\":\"37.jpg\",\"height\":450,\"width\":321,\"price\":21.01,\"rating\":4},{\"title\":\"Smashedavocado\",\"type\":\"fruit\",\"description\":\"Vegansandwicheswithsmashedavocado,tomatoesandradish.topview\",\"filename\":\"38.jpg\",\"height\":450,\"width\":450,\"price\":25.88,\"rating\":0},{\"title\":\"Italianciabatta\",\"type\":\"bakery\",\"description\":\"Italianciabattabreadcutinslicesonwoodenchoppingboardwithherbs,garlicandolivesoverdarkgrungebackdrop,topview\",\"filename\":\"39.jpg\",\"height\":450,\"width\":565,\"price\":15.18,\"rating\":1},{\"title\":\"Rusticbreakfast\",\"type\":\"dairy\",\"description\":\"Rustichealthybreakfastset.cookedbuckwheatgroatswithmilkandhoneyondarkgrungebackdrop.topview,copyspace\",\"filename\":\"40.jpg\",\"height\":450,\"width\":307,\"price\":21.32,\"rating\":0},{\"title\":\"Slicedlemons\",\"type\":\"fruit\",\"description\":\"Heapofwholeandslicedlemonsandlimeswithmintinvintagemetalgridboxoveroldwoodentablewithturquoisewoodenbackground.darkrusticstyle.\",\"filename\":\"41.jpg\",\"height\":600,\"width\":900,\"price\":27.14,\"rating\":2},{\"title\":\"Plums\",\"type\":\"fruit\",\"description\":\"Yellowandredsweetplums\",\"filename\":\"42.jpg\",\"height\":450,\"width\":299,\"price\":19.18,\"rating\":1},{\"title\":\"Frenchfries\",\"type\":\"bakery\",\"description\":\"Homemadeovenbakedfrenchfrieswithketchup\",\"filename\":\"43.jpg\",\"height\":600,\"width\":400,\"price\":18.32,\"rating\":3},{\"title\":\"Strawberries\",\"type\":\"fruit\",\"description\":\"Healthybreakfastset.ricecerealorporridgewithfreshstrawberry,apricots,almondandhoneyoverwhiterusticwoodbackdrop,topview,\\u0000\",\"filename\":\"44.jpg\",\"height\":450,\"width\":352,\"price\":15.13,\"rating\":3},{\"title\":\"Groundbeefmeatburger\",\"type\":\"meat\",\"description\":\"Rawgroundbeefmeatburgersteakcutletswithseasoningonvintagewoodenboards,blackbackground\",\"filename\":\"45.jpg\",\"height\":450,\"width\":675,\"price\":11.73,\"rating\":5},{\"title\":\"Tomatoes\",\"type\":\"fruit\",\"description\":\"Organictomatoesmadewithlove\",\"filename\":\"46.jpg\",\"height\":450,\"width\":675,\"price\":26.03,\"rating\":4},{\"title\":\"Basil\",\"type\":\"vegetable\",\"description\":\"Conceptofveganfoodwithbasil\",\"filename\":\"47.jpg\",\"height\":450,\"width\":678,\"price\":15.19,\"rating\":4},{\"title\":\"Fruitsbouquet\",\"type\":\"fruit\",\"description\":\"Abstractcitrusfruitsbouquetonbluebackground\",\"filename\":\"48.jpg\",\"height\":600,\"width\":401,\"price\":18.18,\"rating\":1},{\"title\":\"Peachesonbranch\",\"type\":\"fruit\",\"description\":\"Peachesonbranchwithleavesandglasseswithpeachjuiceandlimonadewithicecubesinaluminumtrayoveroldmetaltable.darkrusticstyle.topview.\",\"filename\":\"49.jpg\",\"height\":600,\"width\":400,\"price\":25.62,\"rating\":3}]";
+
+
+let parse = JSON.parse(products)
+
+
+
+
+function show() {
+
+
+
+
+    let data = ""
+
+    for (var i = 0; i < parse.length; i++) {
+        data += "<tr> <td>" + i + "</td> <td>" + parse[i].title + "</td> <td>" + parse[i].type + "</td> <td>" + parse[i].description + "</td> <td>" + parse[i].filename + "</td> <td>" + parse[i].height + "</td> <td>" + parse[i].width + "</td> <td>" + parse[i].price + "</td> <td>" + parse[i].rating + "</td> <td> <button onclick='delte(" + i + ")'>" + "delete" + " </button> </td>  <td><button onclick='update(" + i + ")'>" + "update" + "</button></td> <td><button onclick='save(" + i + ")'>" + "save" + "</button></td></tr> "
+
+    }
+
+    document.getElementById('data').innerHTML = data
+}
+
+function delte(deletedIndex) {
+
+
+    parse.splice(deletedIndex, 1);
+    document.getElementById('data').innerHTML = "";
+    show();
+
+}
+
+function update(updatedIndex) {
+
+    var object = parse[updatedIndex];
+    let one = document.getElementById('TITLE');
+    one.value = object.title
+    let two = document.getElementById('TYPE');
+    two.value = object.type
+    let three = document.getElementById('DESCRIPTION');
+    three.value = object.description
+    let four = document.getElementById('FILENAME');
+    four.value = object.filename
+    let five = document.getElementById('HEIGHT');
+    five.value = object.height
+    let six = document.getElementById('WIDTH');
+    six.value = object.width
+    let seven = document.getElementById('PRICE');
+    seven.value = object.price
+    let eigth = document.getElementById('RATING');
+    eigth.value = object.rating
+}
+
+// function update(updateindex) {
+//     var info = {
+//         title: document.getElementById('TITLE').value,
+//         type: document.getElementById('TYPE').value,
+//         description: document.getElementById('DESCRIPTION').value,
+//         filename: document.getElementById('FILENAME').value,
+//         height: document.getElementById('HEIGHT').value,
+//         width: document.getElementById('WIDTH').value,
+//         pricing: document.getElementById('PRICING').value,
+//         rating: document.getElementById('RATING').value,
+//     };
+//     parse.unshift(updateindex, info, 1);
+//     document.getElementById('data').innerHTML = "";
+//     show(info);
+// }
+function save(saveindex) {
+    let save = parse[saveindex];
+    let title = document.getElementById('TITLE').value
+    var type = document.getElementById('TYPE').value
+    var description = document.getElementById('DESCRIPTION').value
+    var filename = document.getElementById('FILENAME').value
+    var height = document.getElementById('HEIGHT').value
+    var width = document.getElementById('WIDTH').value
+    var pricing = document.getElementById('PRICE').value
+    var rating = document.getElementById('RATING').value
+
+    save.title = title;
+    save.type = type;
+    save.description = description;
+    save.filename = filename;
+    save.height = height;
+    save.width = width
+    save.price = pricing
+    save.rating = rating
+
+    document.getElementById('data').innerHTML = "";
+    show();
+}
